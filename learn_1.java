d343c79cd7f30dd3b880073923a65e09af083c14
d.poshekhontsev <d.poshekhontsev@UNISTREAM.COM>
2022-06-20 17:38:25 +0300

    MyPrakticumJava

---
+++ learn_1.java
@@ -0,0 +1,14 @@
+public class learn_1 {
+    public static void main(String[] args) {
+
+        int flatsNumber = 5; // Количество квартир на этаже
+        int floorsNumber = 10; // Количество этажей
+
+        for (int i...; ...; ...) {
+            for (int j...; ...; ...) {
+                int currentFlat = (... - 1) * ... + ...; // Заготовка формулы для учёта квартир
+                System.out.println("Этаж " + ... + " квартира " + ... + " — доставлено.");
+            }
+        }
+    }
+}
